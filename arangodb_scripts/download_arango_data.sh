99fed5cdf1d2f3bcbede088d5cc60cfda63d3c2b
Gergo Petrik <petrikgergo@gmail.com>
2017-11-18 14:32:51 +0100

    arango and orient import script fix

--- arangodb_scripts/download_arango_data.sh
+++ arangodb_scripts/download_arango_data.sh
@@ -15,14 +15,15 @@ if [ ! -f soc-pokec-relationships.txt.gz ]; then
   curl -OL https://snap.stanford.edu/data/soc-pokec-relationships.txt.gz
 fi
 
+#fields must be seperated by tabs
 if [ ! -f soc-pokec-profiles-arangodb.txt ]; then
   echo "Converting PROFILES"
-  echo '_key    public  completion_percentage   gender  region  last_login      registration    AGE  pe      my_eyesight     eye_color       hair_color      hair_type       completed_level_of_education or      love_is_for_me  relation_to_casual_sex  my_partner_should_be    marital_status  children     _to_music       the_idea_of_good_evening        I_like_specialties_from_kitchen fun     I_am_going_tousic    cars    politics        relationships   art_culture     hobbies_interests       science_techns       more'  > soc-pokec-profiles-arangodb.txt
-  gunzip < soc-pokec-profiles.txt.gz | sed -e 's/null//g' -e 's~^~P~' -e 's~    $~~' >> soc-pokec-pro
+  echo '_key	public	completion_percentage	gender	region	last_login	registration	AGE	body	I_am_working_in_field	spoken_languages	hobbies	I_most_enjoy_good_food	pets	body_type	my_eyesight	eye_color	hair_color	hair_type	completed_level_of_education	favourite_color	relation_to_smoking	relation_to_alcohol	sign_in_zodiac	on_pokec_i_am_looking_for	love_is_for_me	relation_to_casual_sex	my_partner_should_be	marital_status	children	relation_to_children	I_like_movies	I_like_watching_movie	I_like_music	I_mostly_like_listening_to_music	the_idea_of_good_evening	I_like_specialties_from_kitchen	fun	I_am_going_to_concerts	my_active_sports	my_passive_sports	profession	I_like_books	life_style	music	cars	politics	relationships	art_culture	hobbies_interests	science_technologies	computers_internet	education	sport	movies	travelling	health	companies_brands	more'  > soc-pokec-profiles-arangodb.txt
+  gunzip < soc-pokec-profiles.txt.gz | sed -e 's/null//g' -e 's~^~P~' -e 's~	$~~' >> soc-pokec-profiles-arangodb.txt
 fi
 
 if [ ! -f soc-pokec-relationships-arangodb.txt ]; then
   echo "Converting RELATIONS"
-  echo '_from   _to' > soc-pokec-relationships-arangodb.txt
-  gzip -dc soc-pokec-relationships.txt.gz | awk -F"\t" '{print "profiles/P" $1 "\tprofiles/P" $2}' >>
+  echo '_from	_to' > soc-pokec-relationships-arangodb.txt
+  gzip -dc soc-pokec-relationships.txt.gz | awk -F"\t" '{print "profiles/P" $1 "\tprofiles/P" $2}' >> soc-pokec-relationships-arangodb.txt
 fi
